c1c6dc7e85bb72d5956ac8a42f172df2dabc5361
jypman <asd029600@gmail.com>
2021-12-12 01:18:41 +0900

    버튼 컴포넌트 import하기

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	export let name;
+	import Button from "./Button.svelte";
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div>
+		<Button btnName="hello! svelte!!"/>
+	</div>
 </main>
 
 <style>
---
+++ src/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let btnName;
+</script>
+
+<main>
+    <button on:click={() => {}}>
+        {btnName}
+    </button>
+</main>
+
+<style>
+
+</style>
